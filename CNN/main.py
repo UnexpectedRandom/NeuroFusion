c3dca1e4e050832faf6cf2304bedef767f05a6bf
UnexpectedRandom <84254521+UnexpectedRandom@users.noreply.github.com>
2025-01-14 07:25:08 -0600

    Update main.py

--- CNN/main.py
+++ CNN/main.py
@@ -43,3 +43,28 @@ class Generative(nn.Module):
         # Output layer
         x = self.output(x)
         return self.tanh(x)
+
+class Discriminator(nn.Module):
+    def __init__(self, input_shape):
+        super(Discriminator, self).__init__()
+        
+        self.model = nn.Sequential(
+            nn.Conv2d(input_shape[0], 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+            
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+            
+            nn.Flatten(),
+            nn.Linear(256 * (input_shape[1] // 8) * (input_shape[2] // 8), 1),
+            nn.Sigmoid() 
+        )
+
+    
+    def forward(self, x):
+        return self.model(x)
